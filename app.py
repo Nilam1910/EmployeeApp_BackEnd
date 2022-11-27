f4413afde66553b830d3ac04717af9f2fe63998c
Nilam1910 <nilampatel@Nilams-MBP.hsd1.il.comcast.net>
2022-11-26 21:20:56 -0600

    fixed app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-# https: // flask.palletsprojects.com/en/2.2.x/quickstart/
 
-from flask import Flask, g
+from flask import Flask, g, jsonify
+from playhouse.db_url import connect
 from flask_cors import CORS
 import models
 from resources.employees import employees
 import os
-# from playhouse.db_url import connect
+from dotenv import load_dotenv
+load_dotenv()  # takes the environment variables from .env
 DEBUG = True
-PORT = os.environ.get('PORT')
+PORT = os.environ.get("PORT")
 
 # Initialize an instance of the Flask class.
 # This starts the website!
 app = Flask(__name__)
 
 
-CORS(employees, origins=['http://localhost:3000',
-     'https://employees-react-demo.demo.herokuapp.com'], supports_credentials=True)  # authentication we need this supports_credentials
+CORS(employees, origins=['https://employee-app-nilam.herokuapp.com/', 'http://localhost:3000',
+                         ], supports_credentials=True)  # authentication we need this supports_credentials
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+
+app.register_blueprint(employees, url_prefix='/api/v1/employees')
+# sets up the directions for handling the routes for the api(employee)
+# The default URL ends in / ("my-website.com/").
 
 
 @app.before_request
@@ -36,11 +41,7 @@ def after_request(response):
     return response
 
 
-app.register_blueprint(employees, url_prefix='/api/v1/employees')
-# sets up the directions for handling the routes for the api(employee)
-# The default URL ends in / ("my-website.com/").
-
-if os.environ.get('FLASK_ENV') != 'development':
+if os.environ.get('FLASK_DEBUG') != 'development':
     print('/non heroku')
     models.initialize()
 # Run the app when the program starts!
